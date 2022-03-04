9219c522da3b9a871a224eb9e1a29898bd16e7a7
Amit Lisha <u8741936@bsmch.net>
2022-03-05 00:40:39 +0200

    add unauthorized interceptor

--- src/Services/AxiosInstance.ts
+++ src/Services/AxiosInstance.ts
@@ -1,6 +1,20 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
-  baseURL: "https://localhost:9000/api/",
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:9000/api/",
   timeout: 2000,
+  withCredentials: true,
 });
+
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+    }
+    window.location.href = "/login";
+  }
+);
+
+export { axiosInstance };
