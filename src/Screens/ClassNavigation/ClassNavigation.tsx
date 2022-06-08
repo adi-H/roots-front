fc94d55df3c59e8b979572d8b7053f621cb663df
Aviv Shopen <AvivShopen1>
2022-06-08 18:50:03 +0300

    feat: Class Navigation screen reworked

--- src/Screens/ClassNavigation/ClassNavigation.tsx
+++ src/Screens/ClassNavigation/ClassNavigation.tsx
@@ -1,4 +1,12 @@
-import { Avatar, Box, Button, Paper, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  Paper,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/material";
 import styles from "./ClassNavigation.module.css";
 import SchoolIcon from "@mui/icons-material/School";
 import { useNavigate } from "react-router-dom";
@@ -6,17 +14,70 @@ import { PageTitle } from "../../Common/PageTitle/PageTitle";
 
 export const ClassNavigation = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
+  const md = theme.breakpoints.up("md");
+  const lg = theme.breakpoints.up("lg");
 
   return (
     <Paper className={styles.container}>
       <PageTitle title="כיתות"></PageTitle>
+      <Stack alignItems="center">
+        <Typography
+          sx={{
+            fontWeight: "600",
+            fontSize: "4rem",
+            [md]: {
+              fontSize: "5rem",
+            },
+            [lg]: {
+              fontSize: "6rem",
+            },
+          }}
+          color="white"
+        >
+          שלום
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: "600",
+            fontSize: "1.25rem",
+            [md]: {
+              fontSize: "1.5rem",
+            },
+            [lg]: {
+              fontSize: "1.75rem",
+            },
+          }}
+          color="white"
+        >
+          מה ברצונך לעשות?
+        </Typography>
+      </Stack>
       <Button
+        sx={{
+          fontSize: "1.25rem",
+          [md]: {
+            fontSize: "1.5rem",
+          },
+          [lg]: {
+            fontSize: "2rem",
+          },
+        }}
         className={styles.navigationButton}
         onClick={() => navigate("/class")}
       >
         השאלת כיתות
       </Button>
       <Button
+        sx={{
+          fontSize: "1.25rem",
+          [md]: {
+            fontSize: "1.5rem",
+          },
+          [lg]: {
+            fontSize: "2rem",
+          },
+        }}
         className={styles.navigationButton}
         onClick={() => navigate("/calendar")}
       >
